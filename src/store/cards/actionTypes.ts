d2c1e27b73e937f461dd5fb0284c77b0b525fb07
Volodymyr Basok <volodymyrquo@gmail.com>
2021-03-01 10:52:20 +0200

    added cards types

---
+++ src/store/cards/actionTypes.ts
@@ -0,0 +1,7 @@
+/* USERS */
+export const GET_CARDS= "ulta-cards/cards/GET_CARDS"
+export const GET_CARDS_SUCCESS =
+  "ulta-cards/cards/GET_CARDS_SUCCESS"
+export const GET_FIELDS_SUCCESS = "ulta-cards/cards/GET_FIELDS_SUCCESS"
+export const IS_FETCHING = "ulta-cards/cards/IS_FETCHING"
+
