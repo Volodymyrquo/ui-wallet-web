82a43a619d287e401534106befad78f09e6838fe
Volodymyr Basok <volodymyrquo@gmail.com>
2021-05-12 13:10:13 +0300

    added icons to project

---
+++ src/pages/PioneerMemberships/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PioneerMemberships = () => {
+    return (
+        <div className="page-content">
+            <h1>Hello from Pioneer Memberships ... </h1>
+        </div>
+    )
+}
+
+export default PioneerMemberships
