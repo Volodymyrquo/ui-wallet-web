448f3f19e08a1727f0227b49f15868b81d7f6fc4
Smikhotur <smikhotur@gmail.com>
2021-05-24 18:18:49 +0300

    Added object currency for testing

---
+++ src/pages/Wallets/currency.ts
@@ -0,0 +1,15 @@
+export const carrencyFiat = [
+  'USD Dollar',
+  'EUR Euro',
+  'PLN Zloty',
+  'GBP British pound',
+  'UAH Hryvnia'
+]
+
+export const carrencyCrypto = [
+  'BTC Bitcoin',
+  'LTC Litecoin',
+  'NMC Namecoinv',
+  'TRX Tronix',
+  'ETH Ethereum'
+]
