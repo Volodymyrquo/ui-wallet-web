b16baf9b50ea58828affc55eae537156790b46b5
Smikhotur <smikhotur@gmail.com>
2021-05-26 17:09:48 +0300

    Fix merge branche

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -10,35 +10,16 @@ import authReducer from "./auth/reducer"
 
 import transactions from "./transactions/reducer"
 
-// import userPhoto from "./userPhoto/reducer"
-// //user settings
-// import userSettings from "./userSettings/reducer"
-// //contacts list
-// import contactsList from "./contactsList/reducer"
-// //cards list
-// import cardsList from "./cards/reducer"
-// //currencies
-// import currencies from "./currencies/reducer"
 //wallets
 import wallets from "./wallets/walletsReducer"
 //referrals
 import referrals from "./referrals/buttonReducer"
-
-
 
 const rootReducer = combineReducers({
   // public
   Layout,
   authReducer,
   transactions,
-  // usersPage,
-  // cardsTypes,
-  // uploadDocuments,
-  // userPhoto,
-  // userSettings,
-  // contactsList,
-  // cardsList,
-  // currencies,
   wallets,
   referrals
 })
